c282c86ecafefee058e520208dbfb2531ae63b58
Cayan6223 <cayan.oezbek@icloud.com>
2026-06-18 17:56:43 +0200

    Fix supabase quotes

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://wkqsjcrqapvuwtlapnvz.supabase.co'
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrcXNqY3JxYXB2dXd0bGFwbnZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3ODYxMTUsImV4cCI6MjA5NzM2MjExNX0.X6IL3O5k79bbg1smM_dVEtTFeHMGWLQ6jk-wFNiNYVU
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrcXNqY3JxYXB2dXd0bGFwbnZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3ODYxMTUsImV4cCI6MjA5NzM2MjExNX0.X6IL3O5k79bbg1smM_dVEtTFeHMGWLQ6jk-wFNiNYVU'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
